5d35bf3357fd300c0b6542c4b6a65e494174a419
809512899 <igalagianand030@gamil.co>
2025-04-22 09:29:11 +0530

    saved

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo {
+    public static void main(String[] args) {
+        int a=10;
+
+        int b=20;
+        int c=a+b;
+        System.out.println("Sum of a and b is: " + c);
+        System.out.println("Simple java program");
+        System.out.println("MAIN");
+        System.out.println("Master");
+
+        
+    }
+
+}
